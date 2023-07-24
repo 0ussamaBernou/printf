b36342003b341e41a5573abbbfe23bba91e934d7
0ussamaBernou <oussama_bernou@um5.ac.ma>
2023-07-24 12:48:15 +0100

    first try

---
+++ _printf.c
@@ -0,0 +1,101 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c) { return (write(1, &c, 1)); }
+
+int print_d(int n)
+{
+	int digits[10];
+	int i, j, written;
+
+	i = 0;
+	written = 0;
+
+	if (n == 0)
+		written += _putchar('0');
+
+	if (n < 0)
+	{
+		written += _putchar('-');
+		n *= -1;
+	}
+
+	while (n != 0)
+	{
+		digits[i] = n % 10;
+		n /= 10;
+		i++;
+	}
+
+	for (j = i - 1; j >= 0; j--)
+	{
+		written += _putchar(digits[j] + '0');
+	}
+	return (written);
+}
+
+int print_s(char *str)
+{
+	int printed;
+	char *string;
+
+	while (*string != '\0')
+	{
+		printed += write(1, string, 1);
+	}
+	printed = 0;
+
+	return (printed);
+}
+
+int _printf(const char *format, ...)
+{
+	va_list ap;
+	int chars_printed, i;
+	char *current_char;
+
+	if (format == NULL)
+	{
+		return (0);
+	}
+
+	current_char = (char *)format;
+	va_start(ap, format);
+	i = 0;
+	while (current_char[i] != '\0')
+	{
+		/* if current char is'%' */
+		if (current_char[i] == '%')
+		{
+			/* handle next char approprietly */
+			switch (current_char[i])
+			{
+			case 'c':
+				chars_printed += _putchar(va_arg(ap, char));
+				break;
+			case 's':
+				chars_printed += print_s(va_arg(ap, char *));
+			case '%':
+				chars_printed += _putchar('%');
+			case 'd':
+				chars_printed += print_d(va_arg(ap, int));
+				break;
+			default:
+				break;
+			}
+		}
+		else
+		{
+			_putchar(current_char[i]);
+		}
+	}
+
+	return chars_printed;
+}
